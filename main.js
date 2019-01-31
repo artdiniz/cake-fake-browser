5114a90da79fb3b441a8ea6f6b9834a2d7a5c7cb
Artur Diniz Adam <artur.daz@gmail.com>
2019-01-31 21:58:44 -0200

    Amnesic features

--- main.js
+++ main.js
@@ -3,10 +3,8 @@ const {app, BrowserWindow} = require('electron')
 function createWindow () {
 
     const {screen} = require('electron')
-    
     const screenSize = screen.getPrimaryDisplay().size
 
-    // Create the browser window.
     mainWindow = new BrowserWindow({
         width: screenSize.width
         ,height: screenSize.height
@@ -16,16 +14,16 @@ function createWindow () {
         ,sandbox: true
     })
 
-    // e carrega index.html do app.
     mainWindow.loadFile('browser/index.html')
 
     const session = mainWindow.webContents.session
 
-    session.clearStorageData(function() {
-        console.log("Before opening: Cleared Caches")
-    })
+    const accessedOrigins = new Set()
 
-    session.webRequest.onHeadersReceived({}, (details, callback) => {        
+    session.webRequest.onHeadersReceived({}, (details, callback) => {
+        
+        accessedOrigins.add(new URL(details.url).origin)
+        
         const originalCSPHeader = details.responseHeaders['Content-Security-Policy']
 
         const bannedPolicyNames = ['frame-ancestors', 'frame-src', 'default-src']
@@ -34,15 +32,15 @@ function createWindow () {
             .reduce((matchedPrevious, doesMatchCurrentPolicy, x) => (matchedPrevious || doesMatchCurrentPolicy), false)
         
         if(originalCSPHeader) {
-            console.log("Original:", JSON.stringify(originalCSPHeader, null, 4))
+            // console.log("Original:", JSON.stringify(originalCSPHeader, null, 4))
 
             const newCSPHeader = details.responseHeaders['Content-Security-Policy']
                 .map(policy => policy.split(";"))
                 .filter(Boolean)
-                .map(props => props.filter(prop => (console.log(prop), !isBannedPolicy(prop))))
+                .map(props => props.filter(prop => !isBannedPolicy(prop)))
                 .map(props => props.join(";"))
             
-            console.log("Filtered:", JSON.stringify(newCSPHeader, null, 4))
+            // console.log("Filtered:", JSON.stringify(newCSPHeader, null, 4))
 
             details.responseHeaders['Content-Security-Policy'] = newCSPHeader
         }
@@ -54,19 +52,35 @@ function createWindow () {
             delete details.responseHeaders['Access-Control-Allow-Origin'];
         }
         callback({cancel: false, responseHeaders: details.responseHeaders});
-    });
+    })
 
     mainWindow.on('close', function() {
         const session = mainWindow.webContents.session
-        session.clearStorageData(function() {
-            console.log("Before exit: Cleared Caches")
-        })
+        console.log("––––––––––––––– BEGIN BEFORE EXIT")
+
+        const clearStorageDataAsync = Promise.all(
+            Array.from(accessedOrigins)
+            .map(origin => new Promise((resolve, reject) => 
+                session.clearStorageData(
+                    {origin: origin}
+                    ,function(){
+                        resolve(`Cleared all storge info for "${origin}"`)
+                    }
+                )
+            ))
+        )
+
+        const printLogsPromise = clearStorageDataAsync.then(logs => logs.forEach(msg => console.log(msg)))
+
+        console.log("–––––––––––––––  END BEFORE EXIT")
+
+        return printLogsPromise
     })
 
     mainWindow.on('closed', function() {
         mainWindow.removeAllListeners()
         mainWindow = null;
-    });
+    })
 }
 
 
