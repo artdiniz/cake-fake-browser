c83a3f68ac826a300c34b72eec19dc1b263c8e86
Artur Diniz Adam <artur.daz@gmail.com>
2019-02-19 12:39:57 -0300

    Fix using this own modules pkh json file for global installs

--- dist/bin.js
+++ dist/bin.js
@@ -1,10 +1,21 @@
 #!/usr/bin/env node
 
-const npm = require('global-npm')
+// Looking for this modules own package.json file and running its start script via npm
+// We run the start script so we can run electron installed as a dependency.
+// With electron as dependency, we don't need to have a build step nor a publish method for the binaries
+// We can just `npm install -g` and run the application via CLI
 
-npm.load({}, (err) => {
+const npm = require('global-npm')
+const readPkgUp = require('read-pkg-up')
+const path = require('path')
+
+const pkgJSONPath =  readPkgUp.sync({cwd: __dirname}).path
+const cakePackageDir = path.dirname(pkgJSONPath)
+
+npm.load({prefix: cakePackageDir}, (err) => {
     if(err) throw err
     npm.commands["run-script"](["start", ...process.argv.slice(2)], (err) => {
         if(err) throw err
     })
 })
+
