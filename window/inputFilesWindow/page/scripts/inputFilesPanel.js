a9d52287c85fe2eaf6a925f8c30df0a9a94852e6
Artur Diniz Adam <artur.daz@gmail.com>
2019-02-28 15:16:17 -0300

    Improvements in ux for inpjt files page

--- window/inputFilesWindow/page/scripts/inputFilesPanel.js
+++ window/inputFilesWindow/page/scripts/inputFilesPanel.js
@@ -4,15 +4,29 @@
     const inputFilesButton = document.querySelector('.inputFilesPanel-button')
     const inputFilesPathView = document.querySelector('.inputFilesPanel-pathView')
 
+    let isLoading = false
+
     inputFilesButton.addEventListener('click', () => {
-        inputPanel.focus()
-        inputPanel.classList.add('inputFilesPanel--active')
-        const folder = (alert('Prompting for files'), 'a/nice/folder ')
+        if(!isLoading) {
+            inputPanel.classList.add('inputFilesPanel--loading')
+            isLoading = true
 
-        inputFilesButton.textContent = 'Carregando...'
-
-        inputFilesPathView.textContent = folder;
+            const folder = (alert('Prompting for files'), 'a/nice/folder ')
+            
+            inputFilesButton.textContent = 'Carregando...'
+            inputFilesButton.classList.add('inputFilesButton--disabled')
+            
+            
+            inputFilesPathView.textContent = folder
+            inputFilesPathView.focus()
+        }
     })
 
+    inputFilesButton.addEventListener('focus', (event) => {
+        if(isLoading) {
+            event.preventDefault()
+            inputFilesPathView.focus()
+        }
+    })
 
 })()
