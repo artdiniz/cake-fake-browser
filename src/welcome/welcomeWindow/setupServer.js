e300a0f1affabd37e6b1f01922a6a5de34ba8418
artdiniz <artur.daz@gmail.com>
2019-07-30 17:41:57 -0300

    Remove useless welcome window server

--- src/welcome/welcomeWindow/setupServer.js
+++
@@ -1,21 +0,0 @@
-import express from 'express'
-import getPort from 'get-port'
-
-export const setupServer = async () => {
-    const server = express()
-
-    server.use(express.static('./page'))
-    
-    const serverAddress = await new Promise(async (resolve, reject) => {
-        const port = await getPort({port: 3000})
-
-        server.listen(port, () => {
-            resolve(`http://localhost:${server.port}`)
-        })
-    })
-
-    
-    return {
-        getAdress: () => serverAddress
-    }
-}
